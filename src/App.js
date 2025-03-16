fb54bafc2ec8db6b891adecc1660de6bbe553db7
Naman Omar <142042135+namanomar@users.noreply.github.com>
2025-03-17 00:12:56 +0530

    update

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+import GameState from './components/GameState';
+
+const App = () => {
+  return (
+    <div className='container'>
+      <GameState />
+    </div>
+  );
+};
+
+export default App;
